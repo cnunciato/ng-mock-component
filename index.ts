5d1d4db66d2300cdc0e6733e6542dc910e0801b1
almar <almarmail-info@yahoo.com>
2016-12-30 15:55:11 +0100

    Add EventEmitters to outputs, fixing 'Cannot read property 'subscribe' of undefined'

--- index.ts
+++ index.ts
@@ -15,12 +15,12 @@ export function MockComponent(options: Component): Component {
     selector: options.selector,
     template: options.template || '',
     inputs: options.inputs,
-    outputs: options.outputs
+    outputs: options.outputs || []
   };
 
   class Mock {}
 
-  options.outputs.forEach((method) => {
+  metadata.outputs.forEach((method) => {
     Mock.prototype[method] = new EventEmitter<Object>();
   });
 
